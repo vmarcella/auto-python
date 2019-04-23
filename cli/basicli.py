36baf187fd31bea60425937fe2e29ec1b99e0a18
C3NZ <cenz@cenz.io>
2019-04-23 09:21:47 -0700

    [feat] add a flag for adding characters

--- cli/basicli.py
+++ cli/basicli.py
@@ -1,12 +1,13 @@
 import argparse
 
 
-def main(number):
-    print("#" * number)
+def main(character, number):
+    print(character * number)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("number", type=int, help="A number")
+    parser.add_argument("-c", type=str, help="Character to print", default="#")
     args = parser.parse_args()
-    main(args.number)
+    main(args.c, args.number)
