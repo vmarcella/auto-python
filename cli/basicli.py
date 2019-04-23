66774c4ef1942858f75f4a87e76878f6990bec36
C3NZ <cenz@cenz.io>
2019-04-23 10:40:52 -0700

    [fix] document the cli tool

--- cli/basicli.py
+++ cli/basicli.py
@@ -1,13 +1,35 @@
+"""
+    Basic CLI application
+"""
 import argparse
 
 
 def main(character, number):
+    """
+        Main function
+    """
     print(character * number)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("number", type=int, help="A number")
+    # Add optional parameters to our commandline program.
     parser.add_argument("-c", type=str, help="Character to print", default="#")
+    parser.add_argument(
+        "-U",
+        action="store_true",
+        default=False,
+        dest="uppercase",
+        help="Uppercase the character",
+    )
+
+    # Parse the args passed into our program
     args = parser.parse_args()
+
+    # Check if the uppercase flag was used
+    if args.uppercase:
+        args.c = args.c.upper()
+
+    # Call main functionality
     main(args.c, args.number)
