7bed8c627f7f0a4c4622dc3f3389879da9da76b3
C3NZ <cenz@cenz.io>
2019-04-24 23:14:40 -0700

    [feat] add an option for the output to be put into a save file

--- automation/simpletask.py
+++ automation/simpletask.py
@@ -3,9 +3,9 @@ import configparser
 import sys
 
 
-def main(number, other_number):
+def main(number, other_number, output):
     result = number * other_number
-    print(f"The result is {result}")
+    print(f"The result is {result}", file=output)
 
 
 if __name__ == "__main__":
@@ -15,8 +15,17 @@ if __name__ == "__main__":
     # Add arguments
     parser.add_argument("-n1", type=int, help="A number", default=1)
     parser.add_argument("-n2", type=int, help="Another number", default=1)
+
+    # argparse.Filetype will open up a file in read mode
     parser.add_argument(
         "--config", "-c", type=argparse.FileType("r"), help="config file"
+    )
+    parser.add_argument(
+        "-o",
+        dest="output",
+        type=argparse.FileType("w"),
+        help="output file",
+        default=sys.stdout,
     )
 
     # parse arguments passed in
@@ -31,4 +40,4 @@ if __name__ == "__main__":
         args.n1 = int(config["ARGUMENTS"]["n1"])
         args.n2 = int(config["ARGUMENTS"]["n2"])
 
-    main(args.n1, args.n2)
+    main(args.n1, args.n2, args.output)
