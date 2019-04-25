dd09c3533c3563beff10a40e375c93c2aae81219
C3NZ <cenz@cenz.io>
2019-04-25 13:08:35 -0700

    [feat] add a formatted version of our task that utilizes a default config if none is provided

---
+++ automation/cron.py
@@ -0,0 +1,50 @@
+import argparse
+import configparser
+import sys
+from datetime import datetime
+
+
+def main(number, other_number, output):
+    """
+        Main task
+    """
+    result = number * other_number
+    print(f"[{datetime.utcnow().isoformat()}] The result is: {result}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    # Config specification
+    parser.add_argument(
+        "-c",
+        "--config",
+        dest="config",
+        type=argparse.FileType("r"),
+        help="task config file",
+        default="/etc/automate.ini",
+    )
+
+    # Output destination
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        type=argparse.FileType("a"),
+        help="The output destination for the task",
+        default=sys.stdout,
+    )
+
+    # Parse our arguments
+    args = parser.parse_args()
+
+    # Load the config
+    if args.config:
+        config = configparser.ConfigParser()
+        config.read_file(args.config)
+        args.n1 = int(config["ARGUMENTS"]["n1"])
+        args.n2 = int(config["ARGUMENTS"]["n2"])
+
+    main(args.n1, args.n2, args.output)
