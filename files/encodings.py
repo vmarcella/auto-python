6ac517c5f5c170d2b38c2dae92f9438a63e068e5
C3NZ <cenz@cenz.io>
2019-05-19 00:33:03 -0700

    [feat] write utf8 text into an iso encoded file

--- files/encodings.py
+++ files/encodings.py
@@ -8,6 +8,7 @@ def read_utf8() -> None:
     """
         Read a utf-8 encoded file
     """
+    logging.info("---READING UTF8 FILE---")
     with open("example_utf8.txt") as file:
         logging.info(file.read())
 
@@ -16,8 +17,30 @@ def read_iso() -> None:
     """
         Read an iso encoded file
     """
+    logging.info("---READING ISO FILE---")
     # Specify the encoding of the file (default is utf-8)
     with open("example_iso.txt", encoding="iso-8859-1") as file:
+        logging.info(file.read())
+
+
+def write_utf_into_iso() -> None:
+    """
+        Write utf-8 encoded content into an iso encoded file
+    """
+    with open("example_utf8.txt") as file:
+        content = file.read()
+
+        with open("example_output_iso.txt", "w", encoding="iso-8859-1") as iso_file:
+            iso_file.write(content)
+
+
+def read_iso_output_file() -> None:
+    """
+        Read the iso output file from the write_utf_into_iso
+        function
+    """
+    logging.info("---READING OUTPUT ISO FILE---")
+    with open("example_output_iso.txt", encoding="iso-8859-1") as file:
         logging.info(file.read())
 
 
@@ -25,3 +48,5 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(message)s")
     read_utf8()
     read_iso()
+    write_utf_into_iso()
+    read_iso_output_file()
