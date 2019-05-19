b247acdbfd4f6984857976c2b7b944ebdb16c359
C3NZ <cenz@cenz.io>
2019-05-19 00:08:40 -0700

    [fix] fix the output format

--- files/zen.py
+++ files/zen.py
@@ -5,7 +5,7 @@ def read_everything() -> None:
     """
         Read everything from the file
     """
-    logging.info("---READING FILE FOR EVERYTHING---\n")
+    logging.info("---READING FILE FOR EVERYTHING---")
     with open("zen_of_python.txt") as file:
         for line in file:
             logging.info(line)
@@ -15,7 +15,7 @@ def read_should() -> None:
     """
         Look for the word should within the file
     """
-    logging.info("---READING FILE FOR SHOULD---\n")
+    logging.info("\n---READING FILE FOR SHOULD---")
     with open("zen_of_python.txt") as file:
         for line in file:
             if "should" in line.lower():
@@ -26,13 +26,17 @@ def read_better() -> None:
     """
         Look for the word better within the file
     """
-    for line in file:
-        if "better" in line.lower():
-            logging.info(line)
-            break
+    logging.info("---READING FILE FOR BETTER---")
+    with open("zen_of_python.txt") as file:
+        for line in file:
+            if "better" in line.lower():
+                logging.info(line)
+                break
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    LOG_FORMAT = "%(message)s"
+    logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
     read_everything()
     read_should()
+    read_better()
