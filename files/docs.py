74640605ad81d88d20e6889b873c8bdf180ac8d6
C3NZ <cenz@cenz.io>
2019-05-27 17:02:39 -0700

    [feat] finish working with docx files

--- files/docs.py
+++ files/docs.py
@@ -11,11 +11,14 @@ def main() -> None:
     """
         Work with docx formatted files
     """
-    # Examining the document
+    # Examining the document meta data. Not all docx generating programs
+    # produce metadata about the files
     doc = docx.Document("document-1.docx")
     logging.info(f"title: {doc.core_properties.title}")
     logging.info(f"Key words: {doc.core_properties.keywords}")
     logging.info(f"Last modified {doc.core_properties.modified}")
+
+    # Data of the document are stored within paragraphs, and not pages
 
 
 if __name__ == "__main__":
